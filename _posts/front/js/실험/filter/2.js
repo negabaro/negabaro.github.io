d4267c08e1cdfe3daa8bce8e7d9d3b1828575213
negabaro kim <negabaro@gmail.com>
2019-12-26 20:14:03 +0900

    remove logic

--- _posts/front/js/실험/filter/2.js
+++ _posts/front/js/실험/filter/2.js
@@ -1,4 +1,5 @@
-const k_code = [1,3]
+
+const k_code = [1,3,4]
 const items = [{ code: 1, label: "リンゴ" }, { code: 2, label: "バナナ" } , 
 { code: 3 , label: "メロン" }, { code: 4, label: "バナナ" } ];
 
@@ -9,3 +10,13 @@ const result = items.filter(function( item){
 
 })
 console.log( result );
+
+
+const y_target = 1;
+const result2 = k_code.filter(function(k){
+    return y_target !== k
+    //1  
+
+})
+
+console.log( result2 );
